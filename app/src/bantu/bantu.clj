26ed94b68ceabe8eeeaaea66abd530b5cbdb02a7
keychera <kyogatama@gmail.com>
2022-12-28 12:33:49 +0700

    async http channels

--- app/src/bantu/bantu.clj
+++ app/src/bantu/bantu.clj
@@ -1,14 +1,15 @@
 (ns bantu.bantu
   (:require [clojure.core.match :as match]
-            [clojure.java.io :as io]
+            [clojure.java.io :as io] 
             [clojure.string :as str]
             [funrepo.anki :as anki]
+            [org.httpkit.server :refer [as-channel send!]]
             [selmer.parser :refer [render-file]]))
 
 (defn app [req]
   {:status  200
    :headers {"Content-Type" "text/html"}
-   :body (render-file "home.html" {:button-text "[Selmer] click me!"})})
+   :body (render-file "home.html" {})})
 
 (defn connect-anki []
   (let [anki-response (try (anki/connect) (catch Exception _ nil))]
@@ -18,11 +19,15 @@
       {:status 200
        :body (render-file "connect-failed.html" {})})))
 
+;; from as-channel source code docs
+(defn anki-async-handler [ring-req]
+  (as-channel ring-req {:on-open (fn [ch]  (send! ch (connect-anki)))}))
+
 ;; https://gist.github.com/borkdude/1627f39d072ea05557a324faf5054cf3
 (defn router [req]
   (let [paths (vec (rest (str/split (:uri req) #"/")))]
     (match/match [(:request-method req) paths]
       [:get []] (app req)
-      [:get ["connect-anki"]] (connect-anki)
+      [:get ["connect-anki"]] (anki-async-handler req)
       [:get ["css" "style.css"]] {:body (slurp (io/resource "public/css/style.css"))}
       :else {:status 404 :body "<p>Page not found.</p>"})))
