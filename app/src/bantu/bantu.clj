2837eb6d5d319f002b26fa1bc8fb55b18c1fbf6c
keychera <kyogatama@gmail.com>
2022-12-31 11:49:02 +0700

    init breadcrumbs

--- app/src/bantu/bantu.clj
+++ app/src/bantu/bantu.clj
@@ -1,6 +1,6 @@
 (ns bantu.bantu
-  (:require [clojure.core.match :as match]
-            [clojure.java.io :as io] 
+  (:require [clojure.core.match :refer [match]]
+            [clojure.java.io :as io]
             [clojure.string :as str]
             [funrepo.anki :as anki]
             [org.httpkit.server :refer [as-channel send!]]
@@ -23,11 +23,34 @@
 (defn anki-async-handler [ring-req]
   (as-channel ring-req {:on-open (fn [ch]  (send! ch (connect-anki)))}))
 
-;; https://gist.github.com/borkdude/1627f39d072ea05557a324faf5054cf3
+;; ;; https://gist.github.com/borkdude/1627f39d072ea05557a324faf5054cf3
+;; (defn router [req]
+;;   (let [paths (vec (rest (str/split (:uri req) #"/")))]
+;;     (match [(:request-method req) paths]
+;;       [:get []] (app req)
+;;       [:get ["connect-anki"]] (anki-async-handler req)
+;;       [:get ["css" "style.css"]] {:body (slurp (io/resource "public/css/style.css"))}
+;;       :else {:status 404 :body "<p>Page not found.</p>"})))
+
+
+;; /
+;; /hello
+;; /doc/intro
+;; /user/chera
+
+(defn hello [] {:body "hello"})
+(defn doc [] {:body "doc"})
+(defn intro [] {:body "intro"})
+(defn user [id] {:body (str "user is " id)})
+
 (defn router [req]
-  (let [paths (vec (rest (str/split (:uri req) #"/")))]
-    (match/match [(:request-method req) paths]
-      [:get []] (app req)
-      [:get ["connect-anki"]] (anki-async-handler req)
+  (let [paths (-> (:uri req) (str/split #"/") rest vec)
+        verb (:request-method req)]
+    (match [verb paths]
+      [:get []] {:body (render-file "ex/bread.html" {})}
+      [verb ["hello"]] (hello)
+      [verb ["doc"]] (doc)
+      [verb ["doc" "intro"]] (intro)
+      [verb ["user" id]] (user id)
       [:get ["css" "style.css"]] {:body (slurp (io/resource "public/css/style.css"))}
-      :else {:status 404 :body "<p>Page not found.</p>"})))
+      :else {:status 404 :body "not found"})))
