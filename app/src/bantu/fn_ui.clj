1cbbc6e4a64f22ef1e21f628f27991b1d539d703
keychera <kyogatama@gmail.com>
2023-01-12 14:46:46 +0700

    capture namespace and fn name error

--- app/src/bantu/fn_ui.clj
+++ app/src/bantu/fn_ui.clj
@@ -3,15 +3,19 @@
             [clojure.string :as str]))
 
 (defn ^{:sidebar "fn"} fn-list-ui [namespace]
-  (require namespace)
-  (let [fns (->> (vals (ns-publics 'funrepo.fns))
-                 (map #(merge {:ref %} (meta %))) (filter :bantu)
-                 (map #(select-keys % [:ref :name]))
-                 (map #(merge % {:ns namespace})))]
-    (render-file "fn/list.html" {:fns fns})))
+  (try
+    (require namespace)
+    (let [fns (->> (vals (ns-publics namespace))
+                   (map #(merge {:ref %} (meta %))) (filter :bantu)
+                   (map #(select-keys % [:ref :name]))
+                   (map #(merge % {:ns namespace})))]
+      (render-file "fn/list.html" {:fns fns}))
+    (catch  Exception e
+      (let [cause (-> e Throwable->map :cause)]
+        (str "<p>" cause "</p>")))))
 
 (defn- fn-ref [ns-symbol-str ref-string]
-  (let [ref (read-string ref-string) 
+  (let [ref (read-string ref-string)
         ns-symbol (read-string ns-symbol-str)]
     (require (eval ns-symbol))
     (meta (eval ref))))
@@ -25,14 +29,17 @@
        (str/join "\n")))
 
 (defn ^{:sidebar "fn"} fn-ui [ref-string]
-  (let [ns-symbol-str (-> (re-matches #"#('.*)\/.*" ref-string) (get 1))
-        ns-name (subs ns-symbol-str 1)
-        ref (fn-ref ns-symbol-str ref-string)
-        {name :name [arglists] :arglists} ref
-        input-ui (arglist->ui arglists)]
-    (render-file "fn/ui.html" {:name name
-                               :post-url (str "/fn/" ns-name "/" name)
-                               :input-ui input-ui})))
+  (try (let [ns-symbol-str (-> (re-matches #"#('.*)\/.*" ref-string) (get 1))
+             ns-name (subs ns-symbol-str 1)
+             ref (fn-ref ns-symbol-str ref-string)
+             {name :name [arglists] :arglists} ref
+             input-ui (arglist->ui arglists)]
+         (render-file "fn/ui.html" {:name name
+                                    :post-url (str "/fn/" ns-name "/" name)
+                                    :input-ui input-ui}))
+       (catch  Exception e
+         (let [cause (-> e Throwable->map :cause)]
+           (str "<p>" cause "</p>")))))
 
 (comment
   (fn-list-ui 'funrepo.fns)
