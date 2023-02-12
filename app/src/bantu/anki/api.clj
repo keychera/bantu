921a0ee6d6e54c394b67ff393fbae5c7540c7c29
keychera <kyogatama@gmail.com>
2023-02-12 14:44:51 +0700

    relevant results UI

--- app/src/bantu/anki/api.clj
+++ app/src/bantu/anki/api.clj
@@ -3,10 +3,11 @@
             [babashka.process :refer [shell]]
             [cheshire.core :as json]
             [clojure.core.async :refer [alt! chan close! thread timeout]]
+            [clojure.edn :as edn]
             [clojure.java.io :as io]
             [clojure.string :as str]
             [org.httpkit.server :refer [as-channel send!]]
-            [selmer.parser :refer [render-file]]))
+            [selmer.parser :refer [render render-file]]))
 
 (defn ^{:sidebar "anki" :title "Anki"} anki [] (render-file "bantu/anki/anki.html" {}))
 
@@ -28,12 +29,22 @@
          {:body (render-file "bantu/anki/failed.html"
                              {:reason (-> e Throwable->map :cause)})})))
 (defn query [input]
-  (curl/get anki-connect
-            {:headers {"Content-Type" "application/json; charset=utf-8"}
-             :body (json/generate-string
-                    {:action "findNotes"
-                     :params {:query (str "deck:current " (some->> input (str/trim-newline)))}})
-             :compressed false}))
+  (-> (curl/get anki-connect
+                {:headers {"Content-Type" "application/json; charset=utf-8"}
+                 :body (json/generate-string
+                        {:action "findNotes"
+                         :params {:query (str "deck:current " input)}})
+                 :compressed false}) :body edn/read-string))
+
+(defn count-unit [number unit]
+  (str number " " unit (when (= number 1) "s")))
+
+(defn relevant-search-html [word]
+  (let [word (-> word str/trim-newline)
+        word-search (-> (render "word:*{{word}}*" {:word word}) query count (count-unit "card"))
+        any-search (-> word query count (count-unit "card"))]
+    (render-file "bantu/anki/result.html" {:word-field word-search
+                                           :any-field any-search})))
 
 (defonce anki-session (atom nil))
 (defonce clipboard-watcher (atom nil))
@@ -55,10 +66,8 @@
           t-ch (let [clip (read-clipboard)]
                  (when-not (= clip last-clip)
                    (println "searching clip =>" clip)
-                   (let [search-via-clip (query clip)]
-                     (send! ws-ch
-                            {:body (str/join [(render-file "bantu/anki/input.html" {:value clip})
-                                              (render-file "bantu/anki/result.html" {:value search-via-clip})])})))
+                   (send! ws-ch {:body (str/join [(render-file "bantu/anki/input.html" {:value clip})
+                                                  (relevant-search-html clip)])}))
                  (recur (inc i) clip (timeout 200))))))
     exit-ch))
 
@@ -84,5 +93,4 @@
                              (some->> (map #(java.net.URLDecoder/decode %)) last))]
     (println "responding query => " search-query)
     (when-not (nil? search-query)
-      (-> (query search-query)
-          (as-> it {:body (str "<div id=\"anki-result\">"  it "</div>")})))))
+      {:body (relevant-search-html search-query)})))
