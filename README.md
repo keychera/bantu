ac935ab9c05ff7ea1d0379c807be5fce0a089f44
keychera <kyogatama@gmail.com>
2023-06-03 14:51:15 +0700

    run as a chrome app

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 a local helper, installable by bbin
 
+run as a chrome app
+```
+bb serve :chrome "<path-to-chrome-executable>"
+```
+
 # devnotes
 
 - using favico.ico has issues (not showing up on chrome, error on firefox). changing it using png works
+- chrome from terminal, --app, and this: https://stackoverflow.com/a/19612982/8812880
--- app/src/serve.clj
+++ app/src/serve.clj
@@ -1,13 +1,18 @@
 (ns serve
-  (:require [bantu.bantu :as bantu] 
+  (:require [babashka.cli :as cli]
+            [babashka.process :as process]
+            [bantu.bantu :as bantu]
+            [clojure.pprint :refer [pprint]]
             [org.httpkit.server :refer [run-server]]))
 
 (defonce port 4242)
 (defonce url (str "http://localhost:" port "/"))
 
 
-(defn -main [& _]
-  (let [router bantu/router]
-    (run-server router {:port port :thread 12})
-    (println "[panas] serving" url)
-    @(promise)))
+(defn -main [& args]
+  (try (let [{:keys [chrome]} (:opts (cli/parse-args args {:require [:chrome]}))
+             router bantu/router]
+         (run-server router {:port port :thread 12})
+         (println "[panas] serving" url)
+         (process/shell chrome (str "--app=" url) (str "--user-data-dir=" (System/getProperty "java.io.tmpdir") "bantu")))
+       (catch Throwable e (pprint (:via (Throwable->map e))))))
